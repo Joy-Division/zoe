8dbbcc187f69353d6faf76a4540a6ef5b98602f2
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-12-19 13:38:55 -0500

    update cdbios

--- work/module/filesys/cdbios_ee.h
+++ work/module/filesys/cdbios_ee.h
@@ -15,21 +15,21 @@ extern "C" {
 void cdbios_init( void );
 
 #if 0 // TODO
-cdbios_callback
-cdbios_get_status
-cdbios_get_read_size
-cdbios_callback_read
-cdbios_read
-cdbios_stop
-cdbios_preseek
-cdbios_get_time
-cdbios_get_filepos
-cdbios_set_hd_file
-send_iop
+cdbios_callback();
+cdbios_get_status();
+cdbios_get_read_size();
+cdbios_callback_read();
+cdbios_read();
+cdbios_stop();
+cdbios_preseek();
+cdbios_get_time();
+cdbios_get_filepos();
+cdbios_set_hd_file();
+send_iop();
 #endif
 
 #ifdef __cplusplus
-} /* extern "C" */
+}
 #endif
 
 #endif /* END OF FILE */
