de657a72a62ae8d6b2445eabdc02cfcf5cf779fd
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2019-12-19 13:38:46 -0500

    update mts

--- work/module/mts/mts.c
+++ work/module/mts/mts.c
@@ -5,8 +5,29 @@
 #include "global.h"
 #include "mts.h"
 
+/*---------------------------------------------------------------------------*/
+
 #if (ZOE_DEMO_OCT2000)
 char MTS_SIG[] = "MTS.KCEJ($Date: 2000/07/31 06:54:23 $)\n"; //! RCS keyword
+#endif
+
+#if (0)
+exeption_dump_func
+threads
+#endif
+
+/*---------------------------------------------------------------------------*/
+
+#if (0)
+mts_exception_dump()
+MTS_SetExceptionCallback()
+MTS_GetThreadId()
+MTS_ExitThread()
+new_thread()
+MTS_NewThread()
+init_threads()
+MTS_BootThread()
+MTS_Panic()
 #endif
 
 /*---------------------------------------------------------------------------*
--- work/module/mts/mts.h
+++ work/module/mts/mts.h
@@ -1,6 +1,6 @@
 /*
  * Multi-Task Scheduler for PS2
- * Public Interface
+ * Public Include
  */
 #ifndef INC_MTS_H
 #define INC_MTS_H
@@ -26,43 +26,51 @@ typedef struct {
 extern "C" {
 #endif
 
+/* ---------------- */
 /* MTS Main (mts.c) */
+/* ---------------- */
 #if 0 // TODO
-MTS_SetExceptionCallback
-MTS_GetThreadId
-MTS_ExitThread
-MTS_NewThread
-MTS_BootThread
-MTS_Panic
+MTS_SetExceptionCallback();
+MTS_GetThreadId();
+MTS_ExitThread();
+MTS_NewThread();
+MTS_BootThread();
+MTS_Panic();
 #endif
 
+/* --------------------------- */
 /* MTS Exception (exception.c) */
+/* --------------------------- */
 void DEBUG_InitException( u_int );
 int cprintf( char *format, ... );
 int printf( char *format, ... );
 
-#if 0 // TODO
-DEBUG_SetExceptionCallback
-DEBUG_ExceptionCallback
-MTS_SetPrintFunc
-excep_printf
+#if (0) // TODO
+DEBUG_SetExceptionCallback();
+DEBUG_ExceptionCallback();
+MTS_SetPrintFunc();
+excep_printf();
 #endif
 
+/* ------------------------------ */
 /* MTS Stack Check (stackcheck.c) */
-#if 0 // TODO
-DEBUG_SetStackCheck
-DEBUG_get_stack_size
-DEBUG_ReleaseStack
-DEBUG_GetStackUseSize
+/* ------------------------------ */
+#if (0) // TODO
+DEBUG_SetStackCheck();
+DEBUG_get_stack_size();
+DEBUG_ReleaseStack();
+DEBUG_GetStackUseSize();
 #endif
 
+/* ------------------------------ */
 /* MTS Exception (exception_in.s) */
-#if 0 // TODO
-DEBUG_ExceptionIn
+/* ------------------------------ */
+#if (0) // TODO
+DEBUG_ExceptionIn();
 #endif
 
 #ifdef __cplusplus
-} /* extern "C" */
+}
 #endif
 
 /*---------------------------------------------------------------------------*/
