f3aef05ac6f567e808698222ca04d1f70b3a4b65
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-02-09 18:58:35 -0500

    update libcm

--- work/common/include/cmconf.h
+++ work/common/include/cmconf.h
@@ -8,11 +8,10 @@
 /*---------------------------------------------------------------------------*/
 /* Data Type Width Specification w/ C Primitives                             */
 /*---------------------------------------------------------------------------*/
-#define CM_TYPE_INT8   char
-#define CM_TYPE_INT16  short
-#define CM_TYPE_INT32  int
-#define CM_TYPE_INT64  long long
-
+#define CM_TYPE_INT8     char
+#define CM_TYPE_INT16    short
+#define CM_TYPE_INT32    int
+#define CM_TYPE_INT64    long long
 #define CM_TYPE_FLOAT32  float
 #define CM_TYPE_FLOAT64  double
 
@@ -21,25 +20,31 @@
 /*---------------------------------------------------------------------------*/
 #if defined( __GNUC__ )
 
-/* 128-bit Floating Point */
-#if defined( __i686__ )\
- || defined( __x86_64__ )
-#define CM_HAVE_FLOAT128 (1)
-#endif /* (ARCH) */
+/* --- 128-bit Floating Point --- */
 
-/* 96-bit Floating Point */
+#if defined( __i686__ )\
+||  defined( __x86_64__ )
+#define CM_HAVE_FLOAT128 (1)
+#define CM_HAVE_MODETF   (1)
+#endif
+
+/* --- 96-bit Floating Point --- */
+
 #if defined( __i686__ )
 #define CM_HAVE_FLOAT96 (1)
-#endif /* (ARCH) */
+#define CM_HAVE_MODEXF  (1)
+#endif
 
-/* 128-bit Integer */
+/* --- 128-bit Integer --- */
+
 #if defined( __x86_64__ )\
- || defined( __R5900__ ) /* (PS2) Emotion Engine */\
- || defined( __psp__ )   /* (PSP) Allegrex       */\
- || defined( __PPU__ )   /* (PS3) Cell/B.E. PPU  */\
- || defined( __SPU__ )   /* (PS3) Cell/B.E. SPU  */
+||  defined( __R5900__ ) /* (PS2) Emotion Engine */\
+||  defined( __psp__ )   /* (PSP) Allegrex       */\
+||  defined( __PPU__ )   /* (PS3) Cell/B.E. PPU  */\
+||  defined( __SPU__ )   /* (PS3) Cell/B.E. SPU  */
 #define CM_HAVE_INT128 (1)
-#endif /* (ARCH) */
+#define CM_HAVE_MODETI (1)
+#endif
 
 #endif /* __GNUC__ */
 
@@ -48,16 +53,15 @@
 /*---------------------------------------------------------------------------*/
 #if defined( __psp__ )
 
-// Check if psptypes.h has been included without suppressing typedefs
-// that conflict with cmtypes.h. Halt the build process if so.
+/* Check if psptypes.h has been included without suppressing typedefs
+ * that conflict with cmtypes.h. Halt the build process if so. */
 #if defined( _SCE_PSPTYPES_H )\
- && !defined( SCE_PSPTYPES_SUPPRESS_ADDITIONAL_DEFINE )
+&& !defined( SCE_PSPTYPES_SUPPRESS_ADDITIONAL_DEFINE )
 #error "psptypes.h was included before conflict suppression!!"
 #endif
 
-// Suppress typedefs conflicting with cmtypes.h
 #ifndef SCE_PSPTYPES_SUPPRESS_ADDITIONAL_DEFINE
-#define SCE_PSPTYPES_SUPPRESS_ADDITIONAL_DEFINE
+#define SCE_PSPTYPES_SUPPRESS_ADDITIONAL_DEFINE (1)
 #endif
 
 #endif /* __psp__ */
--- work/common/include/cmtypes.h
+++ work/common/include/cmtypes.h
@@ -8,7 +8,7 @@
 #include "cmconf.h"
 
 /*---------------------------------------------------------------------------*/
-/* Common Types w/ Unspecified Sign                                          */
+/* Integer Types w/ Unspecified Sign                                         */
 /*---------------------------------------------------------------------------*/
 #ifndef byte
 #define byte     CM_TYPE_INT8
@@ -36,6 +36,9 @@
 #ifndef short16
 #define short16  CM_TYPE_INT16
 #endif
+#ifndef short32
+#define short32  CM_TYPE_INT32
+#endif
 #ifndef long32
 #define long32   CM_TYPE_INT32
 #endif
@@ -44,7 +47,7 @@
 #endif
 
 /*---------------------------------------------------------------------------*/
-/* Common Types w/ Unspecified Width                                         */
+/* Integer Types w/ Unspecified Width                                        */
 /*---------------------------------------------------------------------------*/
 typedef   signed char       schar,  s_char;
 typedef unsigned char       uchar,  u_char;
@@ -58,7 +61,7 @@ typedef   signed long long  sllong, s_llong;
 typedef unsigned long long  ullong, u_llong;
 
 /*---------------------------------------------------------------------------*/
-/* Common Types w/ Specified Width                                           */
+/* Integer Types w/ Specified Width                                          */
 /*---------------------------------------------------------------------------*/
 typedef   signed CM_TYPE_INT8   sbyte,  s_byte;
 typedef unsigned CM_TYPE_INT8   ubyte,  u_byte;
@@ -76,27 +79,32 @@ typedef   signed CM_TYPE_INT8   schar8,   s_char8;
 typedef unsigned CM_TYPE_INT8   uchar8,   u_char8;
 typedef   signed CM_TYPE_INT16  sshort16, s_short16;
 typedef unsigned CM_TYPE_INT16  ushort16, u_short16;
+typedef   signed CM_TYPE_INT32  sshort32, s_short32;
+typedef unsigned CM_TYPE_INT32  ushort32, u_short32;
 typedef   signed CM_TYPE_INT32  slong32,  s_long32;
 typedef unsigned CM_TYPE_INT32  ulong32,  u_long32;
 typedef   signed CM_TYPE_INT64  slong64,  s_long64;
 typedef unsigned CM_TYPE_INT64  ulong64,  u_long64;
 
+/*---------------------------------------------------------------------------*/
+/* Floating Point Types w/ Specified Width                                   */
+/*---------------------------------------------------------------------------*/
 typedef CM_TYPE_FLOAT32  float32, f32;
 typedef CM_TYPE_FLOAT64  float64, f64;
 
-#if defined( __GNUC__ )
-#if defined( CM_HAVE_FLOAT128 )
+#ifdef CM_HAVE_MODETF
 typedef float float128 __attribute__((mode(TF)));
 typedef float f128     __attribute__((mode(TF)));
-#endif /* CM_HAVE_FLOAT128 */
-#if defined( CM_HAVE_FLOAT96 )
+#endif
+#ifdef CM_HAVE_MODEXF
 typedef float float96  __attribute__((mode(XF)));
 typedef float f96      __attribute__((mode(XF)));
-#endif /* CM_HAVE_FLOAT96 */
-#endif /* __GNUC__ */
+#endif
 
-#if defined( __GNUC__ )
-#if defined( CM_HAVE_INT128 )
+/*---------------------------------------------------------------------------*/
+/* 128bit Integer Types (Tetra-Integer Mode)                                 */
+/*---------------------------------------------------------------------------*/
+#ifdef CM_HAVE_MODETI
 typedef /******/ int int128    __attribute__((mode(TI)));
 typedef /******/ int long128   __attribute__((mode(TI)));
 typedef   signed int sint128   __attribute__((mode(TI)));
@@ -109,8 +117,7 @@ typedef   signed int s_long128 __attribute__((mode(TI)));
 typedef unsigned int u_long128 __attribute__((mode(TI)));
 typedef   signed int s128      __attribute__((mode(TI)));
 typedef unsigned int u128      __attribute__((mode(TI)));
-#endif /* CM_HAVE_INT128 */
-#endif /* __GNUC__ */
+#endif
 
 /*---------------------------------------------------------------------------*/
 /* Fake Boolean Types                                                        */
@@ -138,54 +145,85 @@ typedef unsigned int u128      __attribute__((mode(TI)));
 /*---------------------------------------------------------------------------*/
 /* Generic Union Types                                                       */
 /*---------------------------------------------------------------------------*/
+
+/* 8-bit (1 Byte) */
+typedef union union8 {
+	struct {
+		byte bit0:1; /* LSB */
+		byte bit1:1;
+		byte bit2:1;
+		byte bit3:1;
+		byte bit4:1;
+		byte bit5:1;
+		byte bit6:1;
+		byte bit7:1; /* MSB */
+	};
+	s_int8 s8;
+	u_int8 u8;
+} union8;
+
+/* 16-bit (2 Bytes) */
 typedef union union16 {
 	s_int16 s16;
 	u_int16 u16;
 	s_int8  s8[2];
 	u_int8  u8[2];
+	union8  c8[2];
 } union16;
 
+/* 32-bit (4 Bytes) */
 typedef union union32 {
 	s_int32 s32;
 	u_int32 u32;
+	float32 f32;
 	s_int16 s16[2];
 	u_int16 u16[2];
+	union16 c16[2];
 	s_int8  s8[4];
 	u_int8  u8[4];
-	float32 f32;
+	union8  c8[4];
 } union32;
 
+/* 64-bit (8 Bytes) */
 typedef union union64 {
 	s_int64 s64;
 	u_int64 u64;
+	float64 f64;
 	s_int32 s32[2];
 	u_int32 u32[2];
+	float32 f32[2];
+	union32 c32[2];
 	s_int16 s16[4];
 	u_int16 u16[4];
+	union16 c16[4];
 	s_int8  s8[8];
 	u_int8  u8[8];
-	float32 f32[2];
-	float64 f64;
+	union8  c8[8];
 } union64;
 
+/* 128-bit (16 Bytes) */
 typedef union union128 {
 #ifdef CM_HAVE_INT128
 	s_int128 s128;
 	u_int128 u128;
 #endif
-	s_int64  s64[2];
-	u_int64  u64[2];
-	s_int32  s32[4];
-	u_int32  u32[4];
-	s_int16  s16[8];
-	u_int16  u16[8];
-	s_int8   s8[16];
-	u_int8   u8[16];
-	float32  f32[4];
-	float64  f64[2];
 #ifdef CM_HAVE_FLOAT128
 	float128 f128;
 #endif
+	s_int64  s64[2];
+	u_int64  u64[2];
+	float64  f64[2];
+	union64  c64[2];
+	s_int32  s32[4];
+	u_int32  u32[4];
+	float32  f32[4];
+	union32  c32[4];
+	s_int16  s16[8];
+	u_int16  u16[8];
+	union16  c16[8];
+	s_int8   s8[16];
+	u_int8   u8[16];
+	union8   c8[16];
 } union128;
 
 /*---------------------------------------------------------------------------*/
