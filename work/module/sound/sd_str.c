7366bba32787ba437d609f3f74e731bcda57f422
Missingmew <nintendo6496@googlemail.com>
2020-02-13 17:40:03 +0100

    make StrSpuTrans more matchy

--- work/module/sound/sd_str.c
+++ work/module/sound/sd_str.c
@@ -286,7 +286,6 @@ void str_load( void )
 int StrSpuTrans( void )
 {
 	int temp = 0, temp2 = 0;
-	u_int temp3;
 	
 	if( str_stop_fg && str_status > 1 ){
 		switch( str_stop_fg ){
@@ -325,7 +324,7 @@ int StrSpuTrans( void )
 			spu_str_start_ptr_l = 0x6020;
 			sceSdSetAddr( 0x216B, 0x6020 );
 			sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_r, 0x0800 );
-			str_play_offset = 0x0800;
+			str_play_offset += 0x0800;
 			str_unplay_size -= 0x0800;
 			if( !str_mono_fg ){
 				str_read_status[str_play_idx] = 0;
@@ -343,7 +342,7 @@ int StrSpuTrans( void )
 		}
 		if( !str_l_r_fg ){
 			str_trans_buf[str_play_offset+0x07F1] = str_trans_buf[str_play_offset+0x07F1] | 1;
-			sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_l, 0x0800 );
+			sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_l+0x800, 0x0800 );
 			if( !str_mono_fg ){
 				str_play_offset += 0x0800;
 				str_unplay_size -= 0x0800;
@@ -351,7 +350,7 @@ int StrSpuTrans( void )
 			str_l_r_fg = 1;
 		} else {
 			str_trans_buf[str_play_offset+0x07F1] = str_trans_buf[str_play_offset+0x07F1] | 1;
-			sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_r, 0x0800 );
+			sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_r+0x800, 0x0800 );
 			str_play_offset += 0x0800;
 			str_unplay_size -= 0x0800;
 			str_read_status[str_play_idx] = 0;
@@ -367,46 +366,38 @@ int StrSpuTrans( void )
 			str_first_load = 0;
 		}
 		if( str_wait_fg ){
-			// EMPTY
+			break;
+		}
+		if( sound_mono_fg ){
+			sceSdSetParam( 0x0029, (((str_volume * se_pant[0x80]) >> 7) * str_master_vol) / 0x3FFF );
+			sceSdSetParam( 0x0129, (((str_volume * se_pant[0x80]) >> 7) * str_master_vol) / 0x3FFF );
 		} else {
-			if( sound_mono_fg ){
-				temp3 = str_volume * se_pant[0x80];
-				sceSdSetParam( 0x0029, ((temp3 >> 7) * str_master_vol) / 0x3FFF );
-				temp3 = str_volume * se_pant[0x80];
-				sceSdSetParam( 0x0129, ((temp3 >> 7) * str_master_vol) / 0x3FFF );
-			} else {
-				temp3 = str_volume * se_pant[0xFC];
-				sceSdSetParam( 0x0029, ((temp3 >> 7) * str_master_vol) / 0x3FFF );
-				sceSdSetParam( 0x0129, 0 );
-			}
-			temp3 = str_pitch*str_master_pitch;
-			sceSdSetParam( 0x0229, temp3 >> 12 );
-			sceSdSetAddr( 0x2069, 0x5020 );
-			sceSdSetParam( 0x0329, 0xFF );
-			sceSdSetParam( 0x0429, 0x07 );
-			if( sound_mono_fg ){
-				temp3 = str_volume*se_pant[0x80];
-				sceSdSetParam( 0x002B, ((temp3 >> 7)*str_master_vol) / 0x3FFF );
-				sceSdSetParam( 0x012B, ((temp3 >> 7)*str_master_vol) / 0x3FFF );
-			} else {
-				sceSdSetParam( 0x002B, 0 );
-				temp3 = str_volume*se_pant[0xFC];
-				sceSdSetParam( 0x012B, ((temp3 >> 7)*str_master_vol) / 0x3FFF );
-			}
-			temp3 = str_pitch*str_master_pitch;
-			sceSdSetParam( 0x022B, temp3 >> 12 );
-			sceSdSetParam( 0x206B, 0x6020 );
-			sceSdSetParam( 0x032B, 0x00FF );
-			sceSdSetParam( 0x042B, 0x0007 );
-			sceSdSetSwitch( 0x1501, 0x30 );
-			mute_str_l_r_fg = 0;
-			spu_str_idx = 0;
-			str_next_idx = 0x0800;
+			sceSdSetParam( 0x0029, (((str_volume * se_pant[0xFC]) >> 7) * str_master_vol) / 0x3FFF );
+			sceSdSetParam( 0x0129, 0 );
+		}
+		sceSdSetParam( 0x0229, (u_short)((u_int)(str_pitch*str_master_pitch) >> 12) );
+		sceSdSetAddr( 0x2069, 0x5020 );
+		sceSdSetParam( 0x0329, 0xFF );
+		sceSdSetParam( 0x0429, 0x07 );
+		if( sound_mono_fg ){
+			sceSdSetParam( 0x002B, (((str_volume*se_pant[0x80]) >> 7)*str_master_vol) / 0x3FFF );
+			sceSdSetParam( 0x012B, (((str_volume*se_pant[0x80]) >> 7)*str_master_vol) / 0x3FFF );
+		} else {
+			sceSdSetParam( 0x002B, 0 );
+			sceSdSetParam( 0x012B, (((str_volume*se_pant[0xFC]) >> 7)*str_master_vol) / 0x3FFF );
+		}
+		sceSdSetParam( 0x022B, (u_short)((u_int)(str_pitch*str_master_pitch) >> 12) );
+		sceSdSetAddr( 0x206B, 0x6020 );
+		sceSdSetParam( 0x032B, 0x00FF );
+		sceSdSetParam( 0x042B, 0x0007 );
+		sceSdSetSwitch( 0x1501, 0x30 );
+		mute_str_l_r_fg = 0;
+		spu_str_idx = 0;
+		str_next_idx = 0x0800;
+		str_status++;
+		if( !str_unplay_size || (str_unplay_size & 0x80000000) ){
+			str_off_ctr = 0x1F;
 			str_status++;
-			if( str_unplay_size || (str_unplay_size & 0x80000000) ){
-				str_off_ctr = 0x1F;
-				str_status++;
-			}
 		}
 		break;
 	
@@ -418,14 +409,14 @@ int StrSpuTrans( void )
 		}
 		spu_str_idx = sceSdGetAddr( 0x2269 );
 		spu_str_idx -= 0x5020;
-		if( (spu_str_idx < 0x1000) || (spu_str_idx & 0x80000000) ){
+		if( !(spu_str_idx < 0x1000) || (spu_str_idx & 0x80000000) ){
 			break;
 		}
 		if( str_counter_low > spu_str_idx ){
 			str_counter += 0x1000;
 		}
 		str_counter_low = spu_str_idx;
-		if( ((spu_str_idx & 0x0800) == str_next_idx) || str_l_r_fg || mute_str_l_r_fg ){
+		if( (str_next_idx == (spu_str_idx & 0x0800)) || str_l_r_fg || mute_str_l_r_fg ){
 			temp = 1;
 			if( str_read_status[str_play_idx] && !mute_str_l_r_fg ){
 				if( str_mono_fg ){
@@ -453,11 +444,11 @@ int StrSpuTrans( void )
 				} else {
 					str_trans_buf[str_play_offset+0x07F1] = str_trans_buf[str_play_offset+0x07F1] | 1;
 					if( !str_l_r_fg ){
-						sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_l, 0x0800 );
+						sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_l+0x800, 0x0800 );
 						str_l_r_fg = 1;
 					} else {
 						str_next_idx = (str_next_idx+0x0800) & 0x0FFF;
-						sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_r, 0x0800 );
+						sceSdVoiceTrans( 1, 0, str_trans_buf+str_play_offset, (u_char *)spu_str_start_ptr_r+0x800, 0x0800 );
 						str_l_r_fg = 0;
 						str_read_status[str_play_idx] = 0;
 						str_play_idx++;
@@ -469,17 +460,19 @@ int StrSpuTrans( void )
 						}
 					}
 				}
-				if( str_mono_fg && str_mono_offset ){
-					str_play_offset += 0x0800;
-					if( str_play_offset == 0x8000 ){
-						str_play_offset = 0;
-					}
-					if( str_unplay_size > 0x0800 ){
-						str_unplay_size -= 0x0800;
-					} else {
-						str_off_ctr = 0x1F;
-						str_play_offset = 0;
-						str_status++;
+				if( str_mono_fg ) {
+					if( str_mono_offset ){
+						str_play_offset += 0x0800;
+						if( str_play_offset == 0x8000 ){
+							str_play_offset = 0;
+						}
+						if( str_unplay_size > 0x0800 ){
+							str_unplay_size -= 0x0800;
+						} else {
+							str_off_ctr = 0x1F;
+							str_play_offset = 0;
+							str_status++;
+						}
 					}
 				} else {
 					str_play_offset += 0x0800;
@@ -511,11 +504,11 @@ int StrSpuTrans( void )
 					dummy_data[0x07F1] = 3;
 					if( !mute_str_l_r_fg ){
 						mute_str_l_r_fg = 1;
-						sceSdVoiceTrans( 1, 0, dummy_data, (u_char *)spu_str_start_ptr_l, 0x0800 );
+						sceSdVoiceTrans( 1, 0, dummy_data, (u_char *)spu_str_start_ptr_l+0x800, 0x0800 );
 					} else {
 						mute_str_l_r_fg = 0;
 						str_next_idx = (str_next_idx+0x800) & 0x0FFF;
-						sceSdVoiceTrans( 1, 0, dummy_data, (u_char *)spu_str_start_ptr_r, 0x0800 );
+						sceSdVoiceTrans( 1, 0, dummy_data, (u_char *)spu_str_start_ptr_r+0x800, 0x0800 );
 					}
 				}
 			}
