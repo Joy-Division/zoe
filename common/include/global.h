d6b5db289d4eb16a6e74c7ff341a0b35b96c6670
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-04-12 03:12:17 -0400

    Update global.h

--- common/include/global.h
+++ common/include/global.h
@@ -1,11 +1,7 @@
-/*
- * "ZONE OF THE ENDERS"
- * Global Includes & Defines
- */
 #ifndef INCLUDED_GLOBAL_H
 #define INCLUDED_GLOBAL_H
 
-#include "zoe_conf.h" /* Project Config */
-#include "cmtypes.h"  /* LibCM TypeDefs */
+#include "zoe_conf.h"	/* Project Config */
+#include "cmtypes.h"	/* LibCM TypeDefs */
 
 #endif /* END OF FILE */
