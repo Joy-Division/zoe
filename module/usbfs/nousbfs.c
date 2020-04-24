31cac944cb34d7e28c374b4f25c30191ad9698c6
Missingmew <nintendo6496@googlemail.com>
2020-04-24 23:18:14 +0200

    add nousbfs

--- module/usbfs/nousbfs.c
+++ module/usbfs/nousbfs.c
@@ -5,3 +5,38 @@
  */
 /* ver."ZONE OF THE ENDERS"
  */
+
+#include <kernel.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int pcInit() {
+	return 0;
+}
+
+int pcOpen(char *path, int flags) {
+	return open(path, flags);
+}
+
+int pcClose(int fd) {
+	return close(fd);
+}
+
+ssize_t pcRead(int fd, void *buf, size_t count) {
+	return read(fd, buf, count);
+}
+
+ssize_t pcWrite(int fd, void *buf, size_t count) {
+	return write(fd, buf, count);
+}
+
+u_int pcLseek(int fd, u_int offset, int whence) {
+	return lseek(fd, offset, whence);
+}
+
+int start() {
+	extern libhead usbfs_entry;
+	if(RegisterLibraryEntries(&usbfs_entry)) return 1;
+	printf("USB FILE DO NOT USE\n");
+	return 0;
+}
---
+++ module/usbfs/nousbfs.mk
@@ -0,0 +1,61 @@
+#
+# KCEJ USB File System (for PlayStation 2)
+# Copyright (C) Konami Computer Entertainment Japan
+# Codebase Restoration by Joy Division
+#
+# ver."ZONE OF THE ENDERS"
+#
+
+ifeq ($(wildcard PathDefs),)
+PathDefs:
+	iop-path-setup > PathDefs || (rm -f PathDefs ; exit 1)
+endif
+include PathDefs
+
+SCETOP ?= /usr/local/sce
+SCEINCDIR = $(SCETOP)/common/include
+IOPINCDIR = $(SCETOP)/iop/gcc/mipsel-scei-elfl/include
+IOPLIBDIR = $(SCETOP)/iop/gcc/mipsel-scei-elfl/lib
+IOPMODDIR = $(SCETOP)/iop/modules
+
+###############################################################################
+
+INCDIR = ../include
+LIBDIR = ../lib
+
+INCDIRS = -I. -I$(INCDIR) -I$(SCEINCDIR)
+
+ILIBS = 
+
+CFLAGS  = -Wall -G0 $(INCDIRS) -O0 -O1
+ASFLAGS = $(INCDIRS) -G0
+LDFLAGS  = 
+
+PROGNAME = nousbfs.irx
+
+ENTRYFILE = nousbfs_entry.s
+ENTRYTABLE = nousbfs.tbl
+ILBFILE = nousbfs.ilb
+
+OBJS = nousbfs_entry.o nousbfs.o
+
+###############################################################################
+
+all: $(PROGNAME)
+
+clean:
+	-rm -f $(OBJS) $(ENTRYFILE) $(PROGNAME)
+	
+$(ENTRYFILE): $(ENTRYTABLE)
+	$(ILBGEN) -d $(LIBDIR)/$(ILBFILE) -e $(ENTRYFILE) $(ENTRYTABLE)
+	
+nousbfs_entry.o: $(ENTRYFILE)
+
+$(PROGNAME): $(OBJS)
+	$(LINK.o) -o $@ $(OBJS) $(ILIBS)
+
+test: $(PROGNAME)
+	@$(IFIXUP) -o nousbfs-retail-jp.irx $(PROGNAME)
+	@# sha1sum nousbfs-retail-jp.irx
+	@-sha1sum -c nousbfs-retail-jp.sha1
+	@rm nousbfs-retail-jp.irx
