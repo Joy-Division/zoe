27681c8fdaad36c4ec818b944974f139490693ee
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-04-24 21:01:18 -0400

    add usbfs.h

---
+++ module/usbfs/usbfs.h
@@ -0,0 +1,15 @@
+/*
+ * KCEJ USB File System (for PlayStation 2)
+ * Copyright (C) Konami Computer Entertainment Japan
+ * Codebase Restoration by Joy Division
+ */
+/* ver."ZONE OF THE ENDERS"
+ */
+#ifndef INCLUDED_USBFS_H
+#define INCLUDED_USBFS_H
+
+//
+// PLACEHOLDER
+//
+
+#endif /* END OF FILE */
