c83a33211808e212addda2e158ea3d82b3971faf
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-04-24 21:00:30 -0400

    formatting

--- module/usbfs/nousbfs.c
+++ module/usbfs/nousbfs.c
@@ -5,38 +5,47 @@
  */
 /* ver."ZONE OF THE ENDERS"
  */
-
-#include <kernel.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <kernel.h>
 
-int pcInit() {
+int pcInit( void )
+{
 	return 0;
 }
 
-int pcOpen(char *path, int flags) {
-	return open(path, flags);
+int pcOpen( char *path, int flags )
+{
+	return open( path, flags );
 }
 
-int pcClose(int fd) {
-	return close(fd);
+int pcClose( int fd )
+{
+	return close( fd );
 }
 
-ssize_t pcRead(int fd, void *buf, size_t count) {
-	return read(fd, buf, count);
+ssize_t pcRead( int fd, void *buf, size_t count )
+{
+	return read( fd, buf, count );
 }
 
-ssize_t pcWrite(int fd, void *buf, size_t count) {
-	return write(fd, buf, count);
+ssize_t pcWrite( int fd, void *buf, size_t count )
+{
+	return write( fd, buf, count );
 }
 
-u_int pcLseek(int fd, u_int offset, int whence) {
-	return lseek(fd, offset, whence);
+u_int pcLseek( int fd, u_int offset, int whence )
+{
+	return lseek( fd, offset, whence );
 }
 
-int start() {
+int start()
+{
 	extern libhead usbfs_entry;
-	if(RegisterLibraryEntries(&usbfs_entry)) return 1;
-	printf("USB FILE DO NOT USE\n");
+
+	if( RegisterLibraryEntries( &usbfs_entry ) )
+		return 1;
+
+	printf( "USB FILE DO NOT USE\n" );
 	return 0;
 }
