fad17e9b98d10836766f0a48fb736a020ccc86b6
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-05-05 00:18:03 -0400

    Update zoe_conf.h

--- common/include/zoe_conf.h
+++ common/include/zoe_conf.h
@@ -1,9 +1,10 @@
 #ifndef INCLUDED_ZOE_CONF_H
 #define INCLUDED_ZOE_CONF_H
 
-/*---------------------------------------------------------------------------*
- * Definition Check
- *---------------------------------------------------------------------------*/
+/*===========================================================================*/
+/* Definition Check                                                          */
+/*===========================================================================*/
+
 #if (( BUILD_SLPM65019 \
      + BUILD_SLES50111 \
      + BUILD_SLUS20148 \
@@ -16,17 +17,26 @@
 #error "ERROR: 0 or >1 config flags defined."
 #endif
 
-/*---------------------------------------------------------------------------*
- * Alias Definition
- *---------------------------------------------------------------------------*/
-#define ZOE_JPN             BUILD_SLPM65019 // Jan.13,2001
-#define ZOE_PAL             BUILD_SLES50111 // Jan.17,2001
-#define ZOE_USA             BUILD_SLUS20148 // Jan.30,2001
+/*===========================================================================*/
+/* ZONE OF THE ENDERS (for PlayStation 2)                                    */
+/*===========================================================================*/
 
-#define ZOE_DEMO_BORMAN     BUILD_SLPS99999 // Oct.09,2000
-#define ZOE_DEMO_DENGEKI    BUILD_SLPM61002 // Nov.11,2000 :: 電撃PlayStation D40
-#define ZOE_DEMO_PURE2      BUILD_PCPX96611 // Jan.17,2001 :: プレプレ2 Vol.1 2001.04
-#define ZOE_DEMO_USA        BUILD_USA_DEMO  // Jan.23,2001 :: PlayStation Underground
-#define ZOE_DEMO_PAL        BUILD_PAL_DEMO  // Feb.14,2001 :: Official PS2 Magazine
+#define ZOE_DEMO_BORMAN		BUILD_SLPS99999		// Oct.09,2000
+#define ZOE_DEMO_DENGEKI	BUILD_SLPM61002		// Nov.11,2000, 電撃PlayStation D40
+#define ZOE_DEMO_PURE2		BUILD_PCPX96611		// Jan.17,2001, プレプレ2 Vol.1 2001.04
+#define ZOE_DEMO_USA		BUILD_USA_DEMO		// Jan.23,2001, PlayStation Underground
+#define ZOE_DEMO_PAL		BUILD_PAL_DEMO		// Feb.14,2001, Official PS2 Magazine
+
+#define ZOE_JPN				BUILD_SLPM65019		// Jan.13,2001
+#define ZOE_PAL				BUILD_SLES50111		// Jan.17,2001
+#define ZOE_USA				BUILD_SLUS20148		// Jan.30,2001
+
+/*===========================================================================*/
+/* Additional Checks & Defines                                               */
+/*===========================================================================*/
+
+//
+// PLACEHOLDER
+//
 
 #endif /* END OF FILE */
