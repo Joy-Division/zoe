92d8ed97eecb6359227bf494bef35cdc2a740b4b
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-05-05 00:24:30 -0400

    revise eekernel.h defs check

--- source/main/main.h
+++ source/main/main.h
@@ -12,20 +12,24 @@
 // within the libkernl.a static library.
 //
 #if defined(_eekernel_h_)
-#if !defined( INST_CACHE )\
-||  !defined( DATA_CACHE )\
-||  !defined( CacheOn    )\
-||  !defined( CacheOff   )
 
-#define INST_CACHE  2
-#define DATA_CACHE  1
-#define CacheOn()   EnableCache(INST_CACHE | DATA_CACHE)
-#define CacheOff()  DisableCache(INST_CACHE | DATA_CACHE)
+#ifndef INST_CACHE
+#define INST_CACHE	2
+#endif
+#ifndef DATA_CACHE
+#define DATA_CACHE	1
+#endif
+
+#ifndef CacheOn
+#define CacheOn()	EnableCache(INST_CACHE | DATA_CACHE)
+#endif
+#ifndef CacheOff
+#define CacheOff()	DisableCache(INST_CACHE | DATA_CACHE)
+#endif
 
 extern "C" int EnableCache(int);
 extern "C" int DisableCache(int);
 
-#endif // (CACHE DEFS)
 #endif // _eekernel_h_
 
 #endif /* END OF FILE */
