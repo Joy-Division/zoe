2a9028cc086f4dec480e2b7e717abf3655c01a40
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-05-26 00:46:43 -0400

    fix dumb mistake

--- module/include/sound.h
+++ module/include/sound.h
@@ -2,10 +2,10 @@
 #define REDIRECT_SOUND_H
 
 #if defined(__R5900__)
-#  include "../filesys/sd_ee.h"
+#  include "../sound/sd_ee.h"
 #else /* IOP */
-#  include "../filesys/sd_ext.h"
-#  include "../filesys/sd_incl.h"
+#  include "../sound/sd_ext.h"
+#  include "../sound/sd_incl.h"
 #endif
 
 #endif /* END OF FILE */
