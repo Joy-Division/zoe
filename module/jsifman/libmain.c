39d658f3757d9dee9597036b9addbc2cc746b83c
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-05-26 00:51:23 -0400

    jsifman formatting prep

--- module/jsifman/libmain.c
+++ module/jsifman/libmain.c
@@ -1,45 +1,76 @@
+#include <sys/types.h>
 #include <kernel.h>
 #include <sif.h>
 #include <sifcmd.h>
 
-ModuleInfo Module = { "KCEJ_SIF_MANAGER", 0x101 };
+ModuleInfo Module = { "KCEJ_SIF_MANAGER", 0x0101 };
 
-int sif_set_callback_func(unsigned int a0, sceSifCmdHandler a1, void *a2) {
-
+int sif_set_callback_func( u_int a0, sceSifCmdHandler a1, void *a2 )
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_send_packet_and_data() {
-	
+void sif_send_packet_and_data()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void i_sif_send_packet_and_data() {
-	
+void i_sif_send_packet_and_data()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_send_direct() {
-	
+void sif_send_direct()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void i_sif_send_direct() {
-	
+void i_sif_send_direct()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-u_int i_sif_send_mem( u_int *, volatile void *, u_int ) {
-	
+u_int i_sif_send_mem( u_int *a0, volatile void *a1, u_int a2 )
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-u_int sif_send_mem( u_int *, volatile void *, u_int ) {
-	
+u_int sif_send_mem( u_int *a0, volatile void *a1, u_int a2 )
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-int sif_check_status(u_int) {
-	
+int sif_check_status( u_int a0 )
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_init() {
-	
+void sif_init()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-int KCEJ_sifman_inif() {
-	
+int KCEJ_sifman_inif()
+{
+	//
+	// PLACEHOLDER
+	//
 }
--- module/jsifman/rvman.c
+++ module/jsifman/rvman.c
@@ -1,33 +1,58 @@
+#include <sys/types.h>
 #include <kernel.h>
 
-void rv_man_callback() {
-	
+void rv_man_callback()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_init_rv_man() {
-	
+void sif_init_rv_man()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_rv_get_queue() {
-	
+void sif_rv_get_queue()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_rv_call_func() {
-	
+void sif_rv_call_func()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_rv_return_value() {
-	
+void sif_rv_return_value()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void i_sif_rv_return_value() {
-	
+void i_sif_rv_return_value()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-void sif_rv_release_queue() {
-	
+void sif_rv_release_queue()
+{
+	//
+	// PLACEHOLDER
+	//
 }
 
-u_int *sif_get_mem(void *, u_int, u_int) {
-	
+u_int *sif_get_mem( void *a0, u_int a1, u_int a2 )
+{
+	//
+	// PLACEHOLDER
+	//
 }
