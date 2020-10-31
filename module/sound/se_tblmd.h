bce7cfea92e34b8a0ffa21993caa8d7d95622fc2
Jonathan Ingram <26800139+GirianSeed@users.noreply.github.com>
2020-10-30 23:22:07 -0400

    add EU se_data, move nonm* data, define SE IDs

---
+++ module/sound/se_tblmd.h
@@ -0,0 +1,133 @@
+#ifndef INCLUDED_SE_TBLMD_H
+#define INCLUDED_SE_TBLMD_H
+
+#define SE_DUMMY		0
+#define BURNER			1
+#define BEAM1			2
+#define MEGASHOT		3
+#define LASER			4
+#define KEN3			5
+#define BREAK			6
+#define DEF				7
+#define TATE2			8
+#define KENKEN			9
+#define KENUK3			10
+#define BAK				11
+#define TEKIBAKU		12		// 敵爆
+#define KENBAK			13
+#define GLABAK			14
+#define LIGHT			15
+#define KETTEI			16		// 決定
+#define TYAKU			17
+#define DAIBAKU2		18		// 大爆２
+#define BALK			19
+#define KEIKOKU2		20		// 警告２
+#define LEGSCR01		21
+#define LEGSCR02		22
+#define SAKUSYU			23
+#define SYSTEM			24
+#define IGET			25
+#define BAG				26
+#define NAP				27
+#define SHOT2			28
+#define MEGASHO2		29
+#define BAGNAGE			30
+#define BLTYAKU			31
+#define CANON			32
+#define NAGURI2			33		// 殴り２
+#define NAPSYA2			34
+#define TAME2			35
+#define TUKAMI			36		// 掴み
+#define BUILDAI			37
+#define BUILCHU			38
+#define BUILGLA			39
+#define BUILSHO			40
+#define JABERIN			41
+#define GEYSER			42
+#define RAPC4			43
+#define PHALANX			44
+#define HALBERD			45
+#define CHAKLAM			46
+#define DECOY			47
+#define GUNTLET1		48
+#define RYFLE01			49
+#define MAMDEF			50
+#define ITEMHIT1		51
+#define ITEMGET1		52
+#define ITEMBOX1		53
+#define PROGGET1		54
+#define WINOPN01		55
+#define WINCLO01		56
+#define SENTAK01		57		// 選択１
+#define START01			58
+#define CANSEL01		59
+#define CHOWPN01		60
+#define MAMTATE2		61
+#define START02			62
+#define SE_DUMMY_063	63
+#define RECOCH01		64
+#define RECOCH02		65
+#define NOIZE04			66
+#define BUILIR01		67
+#define NOIZE05			68
+#define HALBERD2		69
+#define HALSCR01		70
+#define START03			71
+#define NONAME_072		72
+#define NONAME_073		73
+#define NONAME_074		74
+#define NONAME_075		75
+#define NONAME_076		76
+#define NONAME_077		77
+#define NONAME_078		78
+#define NONAME_079		79
+#define NONAME_080		80
+#define NONAME_081		81
+#define NONAME_082		82
+#define NONAME_083		83
+#define NONAME_084		84
+#define NONAME_085		85
+#define NONAME_086		86
+#define NONAME_087		87
+#define NONAME_088		88
+#define NONAME_089		89
+#define NONAME_090		90
+#define NONAME_091		91
+#define NONAME_092		92
+#define NONAME_093		93
+#define NONAME_094		94
+#define NONAME_095		95
+#define NONAME_096		96
+#define NONAME_097		97
+#define NONAME_098		98
+#define NONAME_099		99
+#define NONAME_100		100
+#define NONAME_101		101
+#define NONAME_102		102
+#define NONAME_103		103
+#define NONAME_104		104
+#define NONAME_105		105
+#define NONAME_106		106
+#define NONAME_107		107
+#define NONAME_108		108
+#define NONAME_109		109
+#define NONAME_110		110
+#define NONAME_111		111
+#define NONAME_112		112
+#define NONAME_113		113
+#define NONAME_114		114
+#define NONAME_115		115
+#define NONAME_116		116		// JABERIN ver.2
+#define NONAME_117		117
+#define NONAME_118		118
+#define NONAME_119		119		// TAME2 ver.2
+#define NONAME_120		120		// CANON ver.2
+#define NONAME_121		121
+#define NONAME_122		122
+#define SE_DUMMY_123	123
+#define SE_DUMMY_124	124
+#define SE_DUMMY_125	125
+#define SE_DUMMY_126	126
+#define SE_DUMMY_127	127
+
+#endif /* END OF FILE */
